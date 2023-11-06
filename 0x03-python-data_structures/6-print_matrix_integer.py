b1bdda88e322de39f54845b51f8723d22e77af4f
iyaz07 <moseseniade@gmail.com>
2023-11-05 23:31:13 -0800

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!usr/bin/pyhton3
+def print_matrix_integer(matrix=[[]]):
+    rowCount = len(matrix)
+
+    for i in range(rowCount):
+        elementCount = len(matrix[i])
+
+        for j in range(elementCount):
+            print('{:d}'.format(matrix[i][j]), end = '')
+            print(end = ' ' if j < (elementCount - 1) else '')
+        print(end ='\n')
