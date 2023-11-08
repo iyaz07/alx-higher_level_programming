f598f4de8d5b7fd1e5bd8fead65b2d3c15c8f7ad
iyaz07 <moseseniade@gmail.com>
2023-11-08 05:59:23 -0800

    Fix the typo

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
     totalSum = 0
 
     for item in my_list:
-        if item not in unique_items:
-            unique_items.add(item)
+        if item not in unique_list:
+            unique_list.add(item)
             totalSum += item
     return (totalSum)
