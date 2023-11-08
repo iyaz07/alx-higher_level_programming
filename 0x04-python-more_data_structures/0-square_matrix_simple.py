e0ac572abc16d150686fcdd6d94b6dbbd7e6b8c4
iyaz07 <moseseniade@gmail.com>
2023-11-08 01:57:33 -0800

    Manipulate matrix and add README

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+def square_matrix_simple(matrix):
+    row_num = len(matrix)
+    col_num = len(matrix[0])
+
+    new_matrix = [[0] * col_num for _ in range(row_num)]
+
+    for i in range(row_num):
+        for j in range(col_num):
+            new_matrix[i][j] = matrix[i][j] ** 2
+    return (new_matrix)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+A Readme file to completes project tasks on Python - More Data Structures: Set, Dictionary
