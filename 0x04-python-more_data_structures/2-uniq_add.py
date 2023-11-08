7d6c25ebedd67ba78dae9bef268d9e4e2d0d0334
iyaz07 <moseseniade@gmail.com>
2023-11-08 05:56:14 -0800

    Fix indented block issue

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,8 +4,7 @@ def uniq_add(my_list=[]):
     totalSum = 0
 
     for item in my_list:
-    if item not in unique_items:
-        unique_items.add(item)
-        totalSum += item
-
+        if item not in unique_items:
+            unique_items.add(item)
+            totalSum += item
     return (totalSum)
