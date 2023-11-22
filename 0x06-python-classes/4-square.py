28f9197c98bc5756fb0ce5042561deb9d64a6b56
iyaz07 <moseseniade@gmail.com>
2023-11-21 21:56:52 -0800

    Define and print the Area of a Square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 4-square.py
+# By Iyaz
+"""Define a class Square."""
+
+
+class Square:
+    """Class variable size"""
+    def __init__(self, size=0):
+        """Initialize size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Check Type and Value of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
