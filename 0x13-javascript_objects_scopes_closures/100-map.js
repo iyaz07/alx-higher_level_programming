cf64652cee372100a89a7b74554fdf98074665af
iyaz07 <moseseniade@gmail.com>
2024-02-13 18:06:07 -0800

    Advanced Task

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const list = require('./100-data.js').list;
+// const newlist = [];
+
+const newlist = list.map((num, index) => {
+  // newlist.push(num * index);
+  return (num * index);
+});
+console.log(list);
+console.log(newlist);#!/usr/bin/node
+const list = require('./100-data.js').list;
+// const newlist = [];
+
+const newlist = list.map((num, index) => {
+  // newlist.push(num * index);
+  return (num * index);
+});
+console.log(list);
+console.log(newlist);
