7c6e11762149d0d121261cb29c6846de6d543242
iyaz07 <moseseniade@gmail.com>
2024-02-14 04:39:08 -0800

    Making corrections

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const size = process.argv.length - 2;
 if (size === 0) {
-	  console.log('No argument');
+  console.log('No argument');
 } else if (size === 1) {
-	  console.log('Argument found');
+  console.log('Argument found');
 } else {
-	  console.log('Arguments found');
+  onsole.log('Arguments found');
 }
