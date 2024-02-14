11566fb6bb0fe57178fcfaff83696ca359a26945
iyaz07 <moseseniade@gmail.com>
2024-02-14 08:05:04 -0800

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
 
-if (size)
-{
-	for (let i = 0; i < size; ++i)
-	{
-		let j = 0;
-		for (; j < size; ++j)
-		{
-			process.stdout.write('X');
-		}
-		if (j === size)
-		{
-			console.log('');
-		}
-	}
+if (size) {
+  for (let i = 0; i < size; ++i) {
+    let j = 0;
+    for (; j < size; ++j) {
+      process.stdout.write('X');
+    }
+    if (j === size) {
+      console.log('');
+    }
+  }
 }
 else
 {
