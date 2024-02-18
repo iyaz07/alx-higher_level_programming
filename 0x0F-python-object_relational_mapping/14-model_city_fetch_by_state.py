d30a9c03ccabd48580c9369e2c752e2c4dae50d2
iyaz07 <moseseniade@gmail.com>
2024-02-18 13:17:16 -0800

    Print city objects

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Prints the first State object from the
+database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access the database and fetch
+    first state
+    """
+
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+
+    # Initialize engine
+    engine = create_engine(db_url)
+
+    # Initialize session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #query
+    query = session.query(State.name, City.id, City.name)
+    query = query.filter(State.id == City.state_id)
+    query = query.order_by(City.id)
+    Element_list = query.all()
+
+    for element in Element_list:
+        print("{}: ({}) {}".format(element[0], element[1], element[2]))
+    Session.close()
