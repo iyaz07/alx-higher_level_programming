e67f5d6b8f29004bf42f5f50f2a6ef34dc67089e
iyaz07 <moseseniade@gmail.com>
2024-02-18 11:28:43 -0800

    working on the ORM Magic

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creates an Instance Base and Class State"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """A Creator for the base State"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
