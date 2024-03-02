34a04d6eae5ebd65c0a52da9bf26fb4de7d9ec9f
iyaz07 <moseseniade@gmail.com>
2024-03-01 18:53:02 -0800

    Display body or handle HTTPError

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+Except HTTPError exceptions, 
+print the HTTP Status Code.
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as url:
+            s = url.read()
+            print(s.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
